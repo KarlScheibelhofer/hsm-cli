6409f36cb161a580f423736da628d6a39992d6ed
KarlScheibelhofer <karl.scheibelhofer.75@gmail.com>
2023-01-07 18:29:34 +0100

    added command completion for algorithms

--- src/main/java/dev/scheibelhofer/CreateCommand.java
+++ src/main/java/dev/scheibelhofer/CreateCommand.java
@@ -11,6 +11,8 @@ import picocli.CommandLine;
                      description = "Create a new key")
 public class CreateCommand implements Runnable {
 
+    enum Algorithm { EC_P256, EC_P384, EC_P521, RSA_PSS_2048, EC_ED25519, EC_ED448;	}
+
     @Inject
     @RestClient
     HsmApiClient hsmApi;
@@ -18,14 +20,14 @@ public class CreateCommand implements Runnable {
     @CommandLine.Option(names = { "-n", "--name" }, description = "key name")
     String name;
 
-    @CommandLine.Option(names = { "-a", "--algorithm" }, description = "key algorithm")
-    String algorithm;
+    @CommandLine.Option(names = { "-a", "--algorithm" }, description = "key algorithm, one of ${COMPLETION-CANDIDATES}")
+    Algorithm algorithm;
 
     @Override
     public void run() {
         HsmApiClient.Key templateKey = new HsmApiClient.Key();
         templateKey.name = name;
-        templateKey.algorithm = algorithm;
+        templateKey.algorithm = algorithm.name();
 
         HsmApiClient.Key key = hsmApi.createKey(templateKey);
 
